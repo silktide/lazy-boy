b9f395e316073379c234e82a60c67f9e2cfa68ab
Doug <dougnelson@silktide.com>
2016-01-19 12:41:04 +0000

    cleaned up applicationmock

--- test/Mocks/MockApplication.php
+++ test/Mocks/MockApplication.php
@@ -13,7 +13,7 @@ class MockApplication extends Application
 
     public function __construct()
     {
-
+        // Very important that it does NOT allow the parent constructor to run
     }
 
     static function reset()
@@ -22,35 +22,31 @@ class MockApplication extends Application
         self::$returns = [];
     }
 
-    /*public function __call($arg1, $arg2)
-    {
+    // Parental overrides
 
-    }*/
     public function offsetGet($id)
     {
-        $value = $this->addFunctionCall("offsetGet", func_get_args());
-        if ($value) {
-            return $value;
-        } else {
-            return parent::offsetGet($id);
-        }
+        return $this->addFunctionCall("offsetGet", func_get_args());
     }
 
     public function offsetSet($offset, $value)
     {
-        $this->addFunctionCall("offsetSet", func_get_args());
+        return $this->addFunctionCall("offsetSet", func_get_args());
     }
 
     public function register(\Pimple\ServiceProviderInterface $provider, array $values = [])
     {
-        //$this->__call("register", func_get_args());
-        $this->addFunctionCall("register", func_get_args());
+        return $this->addFunctionCall("register", func_get_args());
     }
 
+    public function run(Request $request=null)
+    {
+        return $this->addFunctionCall("run", func_get_args());
+    }
 
     public function __call($name, $arguments)
     {
-        $this->addFunctionCall($name, $arguments);
+        return $this->addFunctionCall($name, $arguments);
     }
 
     public function addFunctionCall($name, $arguments)
@@ -62,37 +58,16 @@ class MockApplication extends Application
         self::$calls[$name][] = $arguments;
 
         if (isset(self::$returns[$name])) {
-            //foreach (self::$returns[$name] as $return) {
-                //if ($return[0]==$arguments) {
-                //    return $return[1];
-                //}
-                // return $return == $arguments;
-            //}
             return self::$returns[$name];
         }
     }
 
-/*    public function setReturn($functionName, $args=[], $returnValue)
-    {
-        if (!isset(self::$returns[$functionName])) {
-            self::$returns[$functionName] = [];
-        }
+    // Actual functions that we'll call from outside the class
 
-        self::$returns[$functionName][] = [$args, $returnValue];
-    }
-*/
     public function setReturn($functionName, $returnValue)
     {
         self::$returns[$functionName] = $returnValue;
     }
-
-
-    public function run(Request $request=null)
-    {
-        // STFU
-        $this->addFunctionCall("run", func_get_args());
-    }
-
 
     public function getCalledResponse($functionName)
     {
@@ -106,56 +81,5 @@ class MockApplication extends Application
 
         return array_shift(self::$calls[$functionName]);
     }
-
-    public function getCalled($functionName, $args, $times=1)
-    {
-        if (!isset(self::$calls[$functionName])) {
-            return false;
-        }
-
-        $run = 0;
-        foreach (self::$calls[$functionName] as $call) {
-            if ($call == $args) {
-                $run++;
-            }
-        }
-
-        //var_dump([$run, $times]);
-
-        //var_dump([$run, $times]);
-        //die();
-
-        return ($run == $times);
-    }
-    /*public function register($providerInterface)
-    {
-        self::$calls[] = ["register", func_get_args()];
-    }
-
-    public function offsetGet($id)
-    {
-        self::$calls[] = ["offsetGet", func_get_args()];
-    }*/
-
-    /*public function fuckyou()
-    {
-
-
-        $mockApplication = new MockApplication();
-        $mockApplication->setReturn("offsetGet", $this->routeLoader);
-            //$mockedApplication = \Mockery::mock("Silex\\Application")->shouldIgnoreMissing();
-            //$mockedClassName = get_class($mockedApplication);
-            //MockApplication::mockReset();
-            //MockApplication::setOffsetSet("routeLoader", $this->routeLoader);
-            //$this->application->shouldReceive("register")->with($this->serviceProvider)->times(3);
-            //$this->application->shouldReceive("offsetGet")->with("routeLoader")->once()->andReturn($this->routeLoader);
-
-        $controller = new FrontController($this->builder, "configDir", get_class($mockApplication), $providers);
-        $controller->runApplication();
-
-            //$mockApplication->getCalls();
-        $this->assertTrue($mockApplication->received("register", [$this->serviceProvider], 3));
-        $this->assertTrue($mockApplication->received("offsetGet", [$this->routeLoader]));
-}*/
 }
 
