7854b32ee981f6cb0bffcae88749e4c4960a1081
Danny Smart <downsider84@hotmail.com>
2016-04-05 01:18:44 +0100

    Use the BootableProviderInterface for service providers that need to work on the Application rather than the Container

--- src/Provider/CorsServiceProvider.php
+++ src/Provider/CorsServiceProvider.php
@@ -4,6 +4,7 @@ namespace Silktide\LazyBoy\Provider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,30 +14,33 @@ use Symfony\Component\HttpFoundation\Response;
  *
  * TODO: Review security implications
  */
-class CorsServiceProvider implements ServiceProviderInterface
+class CorsServiceProvider implements ServiceProviderInterface, BootableProviderInterface
 {
 
-    public function register(Container $app) {
-        if ($app instanceof Application) {
-            //handling CORS preflight request
-            $app->before(function (Request $request) {
-                if ($request->getMethod() === "OPTIONS") {
-                    $response = new Response();
-                    $response->headers->set("Access-Control-Allow-Origin","*");
-                    $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-                    $response->headers->set("Access-Control-Allow-Headers","Content-Type,Authorization");
-                    $response->setStatusCode(200);
-                    $response->send();
-                    exit();
-                }
-            }, Application::EARLY_EVENT);
+    public function register(Container $pimple)
+    {
+        // nothing to do here
+    }
 
-            //handling CORS response with right headers
-            $app->after(function (Request $request, Response $response) {
+    public function boot(Application $app) {
+        //handling CORS preflight request
+        $app->before(function (Request $request) {
+            if ($request->getMethod() === "OPTIONS") {
+                $response = new Response();
                 $response->headers->set("Access-Control-Allow-Origin","*");
                 $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-            });
-        }
+                $response->headers->set("Access-Control-Allow-Headers","Content-Type,Authorization");
+                $response->setStatusCode(200);
+                $response->send();
+                exit();
+            }
+        }, Application::EARLY_EVENT);
+
+        //handling CORS response with right headers
+        $app->after(function (Request $request, Response $response) {
+            $response->headers->set("Access-Control-Allow-Origin","*");
+            $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+        });
     }
 
 } 
--- src/Provider/JsonPostServiceProvider.php
+++ src/Provider/JsonPostServiceProvider.php
@@ -4,30 +4,36 @@ namespace Silktide\LazyBoy\Provider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Converts a request body to parameters if it is in JSON format
+ * Automatically decodes JSON request content and adds the values to the Request::request property
  */
-class JsonPostServiceProvider implements ServiceProviderInterface
+class JsonPostServiceProvider implements ServiceProviderInterface, BootableProviderInterface
 {
-    public function register(Container $app) {
-        if ($app instanceof Application) {
-
-            $app->before(function (Request $request) {
-                // check if the content type is JSON
-                if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-                    // decode and check for errors
-                    $data = json_decode($request->getContent(), true);
-                    if (empty($data) && json_last_error() != JSON_ERROR_NONE) {
-                        return new Response('{"error": "The request body was not in JSON format"}');
-                    }
-                    // modify the request with the decoded data
-                    $request->request->add(is_array($data) ? $data : []);
+    public function register(Container $container)
+    {
+        // nothing to do here
+    }
+    
+    public function boot(Application $app)
+    {
+        //accepting JSON
+        $app->before(function (Request $request) {
+            // check if the content type is JSON
+            if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+                // decode and check for errors
+                $data = json_decode($request->getContent(), true);
+                if (empty($data) && json_last_error() != JSON_ERROR_NONE) {
+                    return new Response('{"error": "The request body was not in JSON format"}');
                 }
-            }, Application::EARLY_EVENT + 1); // set priority to be almost always first to run
-        }
+                // modify the request with the decoded data
+                $request->request->add(is_array($data) ? $data : []);
+            }
+            return null;
+        }, Application::EARLY_EVENT + 1); // set priority to be almost always first to run
     }
 } 
