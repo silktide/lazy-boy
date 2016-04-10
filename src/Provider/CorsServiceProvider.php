311cf23b4234d3ec664b3233b3d7d169f432ad33
Danny Smart <downsider84@hotmail.com>
2016-04-11 00:54:15 +0100

    Allow response headers to be exposed to the browser in a CORS request  - refactored header parameter names to be semantic

--- src/Provider/CorsServiceProvider.php
+++ src/Provider/CorsServiceProvider.php
@@ -19,19 +19,22 @@ class CorsServiceProvider implements ServiceProviderInterface, BootableProviderI
 
     public function register(Container $pimple)
     {
-        $pimple["cors.defaultHeaders"] = ["Content-Type", "Authorization"];
-        $pimple["cors.additionalHeaders"] = [];
+        $pimple["cors.request.defaultHeaders"] = ["Content-Type", "Authorization"];
+        $pimple["cors.request.headers"] = [];
+        $pimple["cors.response.headers"] = [];
     }
 
     public function boot(Application $app) {
-        $allowedHeaders = array_merge($app["cors.defaultHeaders"], $app["cors.additionalHeaders"]);
+        $allowedResponseHeaders = $app["cors.response.headers"];
+        $allowedRequestHeaders = array_merge($app["cors.request.defaultHeaders"], $app["cors.request.headers"]);
         //handling CORS preflight request
-        $app->before(function (Request $request) use ($allowedHeaders) {
+        $app->before(function (Request $request) use ($allowedRequestHeaders) {
             if ($request->getMethod() === "OPTIONS") {
                 $response = new Response();
                 $response->headers->set("Access-Control-Allow-Origin","*");
                 $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-                $response->headers->set("Access-Control-Allow-Headers",implode(",", $allowedHeaders));
+                $response->headers->set("Access-Control-Allow-Headers",implode(",", $allowedRequestHeaders));
+
                 $response->setStatusCode(200);
                 $response->send();
                 exit();
@@ -39,9 +42,10 @@ class CorsServiceProvider implements ServiceProviderInterface, BootableProviderI
         }, Application::EARLY_EVENT);
 
         //handling CORS response with right headers
-        $app->after(function (Request $request, Response $response) {
+        $app->after(function (Request $request, Response $response) use ($allowedResponseHeaders) {
             $response->headers->set("Access-Control-Allow-Origin","*");
             $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+            $response->headers->set("Access-Control-Expose-Headers",implode(",", $allowedResponseHeaders));
         });
     }
 
